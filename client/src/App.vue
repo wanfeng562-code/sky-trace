c3239ee577e7b20fea04ecf95ce00230fba70e9c
Breeze <2865167171@qq.com>
2026-04-12 17:00:10 +0800

    清理分支

--- client/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<router-view />
-</template>
-
-<script setup lang="ts">
-	// TODO: Add layout shell (header/sidebar/status bar) when pages increase.
-</script>
--- server/app/state.py
+++
@@ -1,6 +0,0 @@
-from app.services.flight_store import FlightStore
-from app.services.mock_collector import MockCollector
-
-# Shared process-level objects.
-flight_store = FlightStore()
-mock_collector = MockCollector()
